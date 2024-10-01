b814ce9bb7293ae4ac7e528775dd6e9e34f9c9f3
Helena Santos Rezende <99421906+helenahsr@users.noreply.github.com>
2024-10-01 11:49:06 -0300

    Add files via upload

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -41,6 +41,7 @@ public class Principal {
                     "2 - Deletar Livro\n" +
                     "3 - Buscar Livro\n" +
                     "4 - Listar todos os livros\n" +
+                    "5 - Atualizar Livro\n" +
                     "0 - Sair"
             ));
 
@@ -56,14 +57,84 @@ public class Principal {
                 	JOptionPane.showMessageDialog(null, "Livro cadastrado com sucesso!");
                     break;
                 case 2:
-                    
+                	String item = "";
+            		String lista = "";
+            		int index = 0;
+            		if(livros != null)	
+            		{
+            			item = JOptionPane.showInputDialog ("Digite o nome do livro que deseja remover:");
+            			for (Livro l : livros) {
+            				if(l.getTitulo().contains(item)) {
+            					livros.remove(index);
+            					JOptionPane.showMessageDialog(null, "Livro removido!");
+            					break;
+            				}
+            				index++;
+            			}
+            			
+            		}
+            		else {
+            			JOptionPane.showMessageDialog(null, "Não há livros cadastrados!");
+            		}
                     break;
                 case 3:
-                    
+                	String itemBusca = "", busca = "";
+    				int contagem = 0;
+    				try {
+    					busca = JOptionPane.showInputDialog ("Digite o nome do livro que deseja procurar: ");
+    				}
+    				catch(Exception e)
+    				{
+    					JOptionPane.showMessageDialog(null, "Insira um nome válido!");
+    					break;
+    				}
+    				
+    				for (Livro l : livros) {
+    					if(l.getTitulo().contains(busca))
+    					{
+    						itemBusca = "ID: " + l.getId()  + " | Título: " + l.getTitulo() + " | Autor: " + l.getAutor() + " | Número de páginas: " + l.getNumPaginas() + "\n";
+    					}
+    					contagem++;
+    				}
+    				
+    				if(itemBusca != "")
+    				{
+    					JOptionPane.showMessageDialog(null, "Exibindo livro da busca\n" + itemBusca);
+    				}
+    				else
+    				{
+    					JOptionPane.showMessageDialog(null, "Livro não encontrado!");
+    				}
                     break;
                 case 4:
-                    
+                	String lista1 = "";
+    				for (Livro l : livros) {
+    					lista1 += "ID: " + l.getId()  + " | Título: " + l.getTitulo() + " | Autor: " + l.getAutor() + " | Número de páginas: " + l.getNumPaginas() + "\n";
+    				}
+    				if(lista1 != "") {
+    					JOptionPane.showMessageDialog(null, "Exibindo lista de livros:\n" + lista1);
+    				}
+    				else {
+    					JOptionPane.showMessageDialog(null, "Não há livros cadastrados!");
+    				}
                     break;
+                case 5:
+                	int id = 0;
+                	id = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o ID do livro que deseja alterar:"));
+                	for (Livro l : livros)
+                	{
+                		if (l.getId() == id)
+                		{
+                			JOptionPane.showMessageDialog(null, "Item encontrado!\n" + "ID: " + l.getId()  + " | Titulo da obra: " + l.getTitulo() + " | Autor: " + l.getAutor() + " | Número de páginas: " + l.getNumPaginas() + "\n");
+                			l.setId(Integer.parseInt(JOptionPane.showInputDialog("Deseja alterar o ID?", l.getId())));
+                			l.setTitulo(JOptionPane.showInputDialog("Deseja alterar o título?:", l.getTitulo()));
+                			l.setAutor(JOptionPane.showInputDialog("Deseja alterar o autor?", l.getAutor()));
+                			l.setNumPaginas(Integer.parseInt(JOptionPane.showInputDialog("Deseja alterar o número de páginas?", l.getNumPaginas())));
+                			JOptionPane.showMessageDialog(null, "Livro alterado com sucesso!");
+                			break;                			
+                		}
+                	}
+                	break;
             }
         } while (opcao != 0);
 	}
@@ -75,6 +146,7 @@ public class Principal {
                     "2 - Deletar Revista\n" +
                     "3 - Buscar Revista\n" +
                     "4 - Listar todos as revsitas\n" +
+                    "5 - Atualizar revista\n" +
                     "0 - Sair"
             ));
 
@@ -110,7 +182,33 @@ public class Principal {
             		}
                     break;
                 case 3:
-                    
+                	String itemBusca = "", busca = "";
+    				int contagem = 0;
+    				try {
+    					busca = JOptionPane.showInputDialog ("Digite o nome da revista que deseja procurar: ");
+    				}
+    				catch(Exception e)
+    				{
+    					JOptionPane.showMessageDialog(null, "Insira um nome válido!");
+    					break;
+    				}
+    				
+    				for (Revista r : revistas) {
+    					if(r.getTitulo().contains(busca))
+    					{
+    						itemBusca = "ID: " + r.getId()  + " | Título: " + r.getTitulo() + " | Autor: " + r.getAutor() + " | Edição: " + r.getEdicao() + "\n";
+    					}
+    					contagem++;
+    				}
+    				
+    				if(itemBusca != "")
+    				{
+    					JOptionPane.showMessageDialog(null, "Exibindo revista da busca\n" + itemBusca);
+    				}
+    				else
+    				{
+    					JOptionPane.showMessageDialog(null, "Revista não encontrada!");
+    				}
                     break;
                 case 4:
     				String lista1 = "";
@@ -124,6 +222,24 @@ public class Principal {
     					JOptionPane.showMessageDialog(null, "Não há revistas cadastradas!");
     				}
                     break;
+                case 5:
+                	int id = 0;
+                	id = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o ID da revista que deseja alterar:"));
+                	for (Revista r : revistas)
+                	{
+                		if (r.getId() == id)
+                		{
+                			JOptionPane.showMessageDialog(null, "Item encontrado!\n" + "ID: " + r.getId()  + " | Titulo da obra: " + r.getTitulo() + " | Autor: " + r.getAutor() + " | Edição: " + r.getEdicao() + "\n");
+                			r.setId(Integer.parseInt(JOptionPane.showInputDialog("Deseja alterar o ID?", r.getId())));
+                			r.setTitulo(JOptionPane.showInputDialog("Deseja alterar o título?:", r.getTitulo()));
+                			r.setAutor(JOptionPane.showInputDialog("Deseja alterar o autor?", r.getAutor()));
+                			r.setEdicao(Integer.parseInt(JOptionPane.showInputDialog("Deseja alterar a edição?", r.getEdicao())));
+                			JOptionPane.showMessageDialog(null, "Revista alterada com sucesso!");
+                			break;
+                			
+                		}
+                	}                	
+                	break;
             }
         } while (opcao != 0);
 	}
